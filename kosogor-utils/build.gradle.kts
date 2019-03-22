c5789e49c2254001475323b311390af08af18315
vdtankov <vdtankov@gmail.com>
2019-03-22 11:32:47 +0300

    Add publishing of kosogor-utils

--- kosogor-utils/build.gradle.kts
+++ kosogor-utils/build.gradle.kts
@@ -1,4 +1,26 @@
+import tanvd.kosogor.proxy.publishJar
+
+group = "tanvd.kosogor"
+version = "1.0.0"
+
 dependencies {
     compile(gradleKotlinDsl())
     compile(gradleApi())
+}
+
+publishJar {
+    publication {
+        artifactId = "kosogor-utils"
+    }
+
+    bintray {
+        username = "tanvd"
+        repository = "tanvd.kosogor"
+        info {
+            description = "Kosogor Gradle utils"
+            githubRepo = "https://github.com/TanVD/kosogor"
+            vcsUrl = "https://github.com/TanVD/kosogor"
+            labels.addAll(listOf("gradle", "utils", "kotlin", "kotlin-dsl"))
+        }
+    }
 }
