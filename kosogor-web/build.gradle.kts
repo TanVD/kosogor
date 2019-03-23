4aa444b0f006343489ec9d591d851a29c68439e4
Vladislav Tankov <vdtankov@gmail.com>
2019-03-23 16:08:21 +0300

    Move to 1.0.4-SNAPSHOT kosogor-web

--- kosogor-web/build.gradle.kts
+++ kosogor-web/build.gradle.kts
@@ -2,7 +2,7 @@ import tanvd.kosogor.proxy.publishJar
 import tanvd.kosogor.proxy.publishPlugin
 
 group = "tanvd.kosogor.web"
-version = "1.0.3"
+version = "1.0.4-SNAPSHOT"
 
 dependencies {
     compileOnly(gradleKotlinDsl())
