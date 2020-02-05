235e549e6fa83a64510daff6a1ce9a35a0b673e0
Vladislav Tankov <vdtankov@gmail.com>
2020-02-06 02:48:43 +0300

    Move to 1.0.5-SNAPSHOT kosogor.terraform plugin

--- kosogor-terraform/build.gradle.kts
+++ kosogor-terraform/build.gradle.kts
@@ -2,7 +2,7 @@ import tanvd.kosogor.proxy.publishJar
 import tanvd.kosogor.proxy.publishPlugin
 
 group = "tanvd.kosogor.terraform"
-version = "1.0.4"
+version = "1.0.5-SNAPSHOT"
 
 dependencies {
     compileOnly(gradleKotlinDsl())
