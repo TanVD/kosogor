b7b3acd6b05612d64a29739a209ae0588a785329
Olga.Shalganova <olga.shalganova@jetbrains.com>
2020-02-06 17:25:10 +0300

    Improve skip-validation option, don't add provider into module

--- kosogor-terraform/build.gradle.kts
+++ kosogor-terraform/build.gradle.kts
@@ -2,7 +2,7 @@ import tanvd.kosogor.proxy.publishJar
 import tanvd.kosogor.proxy.publishPlugin
 
 group = "tanvd.kosogor.terraform"
-version = "1.0.5-SNAPSHOT"
+version = "1.0.5"
 
 dependencies {
     compileOnly(gradleKotlinDsl())
