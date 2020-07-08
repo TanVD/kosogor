765f2a15cad8f4a80b957a67d5fcc50d0de4df48
Olga.Shalganova <olga.shalganova@jetbrains.com>
2020-07-08 13:35:04 +0300

    Release kosogor-terraform 1.0.7

--- kosogor-terraform/build.gradle.kts
+++ kosogor-terraform/build.gradle.kts
@@ -2,7 +2,7 @@ import tanvd.kosogor.proxy.publishJar
 import tanvd.kosogor.proxy.publishPlugin
 
 group = "tanvd.kosogor.terraform"
-version = "1.0.7-SNAPSHOT"
+version = "1.0.7"
 
 dependencies {
     compileOnly(gradleKotlinDsl())
