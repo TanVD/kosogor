726a4a307991698b2c5046671b39f707d155ea22
Andrey.Tarashevskiy <andrey.tarashevskiy@jetbrains.com>
2020-07-12 23:00:34 +0300

    Move kosogor-web to 1.0.7

--- kosogor-web/build.gradle.kts
+++ kosogor-web/build.gradle.kts
@@ -2,7 +2,7 @@ import tanvd.kosogor.proxy.publishJar
 import tanvd.kosogor.proxy.publishPlugin
 
 group = "tanvd.kosogor.web"
-version = "1.0.6"
+version = "1.0.7-SNAPSHOT"
 
 dependencies {
     compileOnly(gradleKotlinDsl())
