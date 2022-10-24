b1055432e03f3796ad9aafa7883bdde41b8587de
Andrey.Tarashevskiy <andrey.tarashevskiy@jetbrains.com>
2022-10-25 01:22:27 +0300

    Code cleanup

--- kosogor/src/main/kotlin/tanvd/kosogor/utils/PrivateAccessors.kt
+++ kosogor/src/main/kotlin/tanvd/kosogor/utils/PrivateAccessors.kt
@@ -5,7 +5,6 @@ package tanvd.kosogor.utils
 import com.gradle.publish.PluginBundleExtension
 import org.gradle.api.Project
 import org.gradle.api.internal.HasConvention
-import org.gradle.api.plugins.ExtensionAware
 import org.gradle.api.plugins.ExtraPropertiesExtension
 import org.gradle.api.publish.PublishingExtension
 import org.gradle.api.tasks.SourceSetContainer
@@ -27,11 +26,11 @@ internal fun Project._publishing(configure: PublishingExtension.() -> Unit) {
 }
 
 internal fun Project._pluginBundle(configure: PluginBundleExtension.() -> Unit) {
-    (this as ExtensionAware).extensions.configure("pluginBundle", configure)
+    extensions.configure("pluginBundle", configure)
 }
 
 internal fun Project._gradlePlugin(configure: GradlePluginDevelopmentExtension.() -> Unit) {
-    (this as ExtensionAware).extensions.configure("gradlePlugin", configure)
+    extensions.configure("gradlePlugin", configure)
 }
 
 internal inline fun <reified T : Any> Project.extByName(name: String): T = extensions.getByName<T>(name)
